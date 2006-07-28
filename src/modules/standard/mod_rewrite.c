b5ef7c101b077a762f88ffa075f75357d1400eea
sumikawa <sumikawa@c27e49f0-d0dd-db11-86f2-004063dcf3ec>
2006-07-28 11:18:06 +0000

    Sync with 1.3.37

--- src/modules/standard/mod_rewrite.c
+++ src/modules/standard/mod_rewrite.c
@@ -1,8 +1,9 @@
-/* Copyright 1999-2004 The Apache Software Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+/* Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -2735,7 +2736,7 @@ static char *escape_absolute_uri(ap_pool *p, char *uri, unsigned scheme)
             int c = 0;
 
             token[0] = cp = ap_pstrdup(p, cp);
-            while (*cp && c < 5) {
+            while (*cp && c < 4) {
                 if (*cp == '?') {
                     token[++c] = cp + 1;
                     *cp = '\0';
